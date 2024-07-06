6c8c6bb1f00d513328eddb61f5eca86bdcbf4ab8
Anurag Sanyal <anuragsanyal@192.168.1.4>
2024-07-06 22:16:47 +0530

    refactor readability service

--- services/readabilityService.js
+++ services/readabilityService.js
@@ -1,20 +1,15 @@
 const kafkaProducer = require('../kafkaProducer');
 const KafkaConsumer = require('../kafkaConsumer');
-const Readability = require('@mozilla/readability');
-const JSDOM = require('jsdom');
 
 class SchedulerService {
     static async listenToReadabilityServiceTopic() {
         const consumer = await KafkaConsumer.getConsumer({topic: 'readability-service-topic', groupId: 'readability-service-group'});
-        // await consumer.connect();
-        // await consumer.subscribe({ topic: 'readability-service-topic', fromBeginning: true });
-
+        
         await consumer.run({
             eachMessage: async ({ topic, partition, message }) => {
                 const { content, nextStage, retryCount = 1 } = JSON.parse(message.value.toString());
                 
                 try {
-                    console.log(`Processing message in TopicService (Retrycount ${retryCount}):`, content);
                     console.log('simulation readability processing');
 
                     await kafkaProducer.produceMessage('scheduling-topic', {
